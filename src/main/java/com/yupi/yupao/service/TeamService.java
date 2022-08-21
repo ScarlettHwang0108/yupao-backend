6006ab695cc3a7b2f6b658b6ea6b58619cdad299
liyupi <592789970@qq.com>
2022-08-22 00:42:52 +0800

    add 添加组队功能增删改查 add 完成创建队伍功能

---
+++ src/main/java/com/yupi/yupao/service/TeamService.java
@@ -0,0 +1,20 @@
+package com.yupi.yupao.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.yupi.yupao.model.domain.Team;
+import com.yupi.yupao.model.domain.User;
+
+/**
+ *
+ */
+public interface TeamService extends IService<Team> {
+
+    /**
+     * 创建队伍
+     *
+     * @param team
+     * @param loginUser
+     * @return
+     */
+    long addTeam(Team team, User loginUser);
+}
